ab3f8474e04e625ae98d4a85faacfb16fce769a0
Simon Slamka <51794014+simonSlamka@users.noreply.github.com>
2023-11-29 00:16:36 +0100

    quick typo fix

--- app.py
+++ app.py
@@ -83,6 +83,6 @@ iface = gr.Interface(
     inputs="image",
     outputs=["text", "number", "image"],
     title="Attraction Classifier - subjective",
-    description="Takes in a (224, 224) image and outputs an attraction class: {\"pos\", \"neg\"}. Face detection, cropping, and resizing is done internally."
+    description="Takes in a (224, 224) image and outputs an attraction class: {\"pos\", \"neg\"}. Face detection, cropping, and resizing are done internally."
 )
 iface.launch()
