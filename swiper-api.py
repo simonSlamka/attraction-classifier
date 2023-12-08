81d2c0f2124662fdb29abe12293407688088ff78
Simon Slamka <51794014+simonSlamka@users.noreply.github.com>
2023-12-08 01:14:26 +0100

    now works well via the API

--- swiper-api.py
+++ swiper-api.py
@@ -8,12 +8,14 @@ from uuid import uuid4
 import argparse
 from termcolor import colored
 import requests
+from face_grab import FaceGrabber
 from dotenv import load_dotenv
+import numpy as np
 
 load_dotenv()
 
 
-logging.basicConfig(filename="logfile.log", level=logging.INFO)
+logging.basicConfig(level=logging.INFO) #filename="logfile.log")
 
 # constants
 APIURL = "https://api-inference.huggingface.co/models/ongkn/attraction-classifier"
@@ -33,36 +35,49 @@ def main() -> None:
     args = parser.parse_args()
     posDir = args.posDir
     negDir = args.negDir
+    faceGrabber = FaceGrabber()
 
     while True:
-        sleep_time = random.randint(3, 8)
-        logging.info(f"waiting {sleep_time} seconds before proceeding to hopefully prevent rate limiting")
-        sleep(sleep_time)
+        sleepTime = random.randint(2, 4)
+        logging.info(f"waiting {sleepTime} seconds before proceeding to hopefully prevent rate limiting")
+        sleep(sleepTime)
         focus_ffox()
         logging.info("taking a screenshot")
         subprocess.run(["gnome-screenshot", "-w", "-f", "temp_screenshot.png"])
         pwd = os.getcwd()
         imgPath = os.path.join(pwd, "temp_screenshot.png")
-        logging.info(f'Classifying image at {imgPath}')
-        with open(imgPath, "rb") as f:
-            img = f.read()
-        result = requests.post(APIURL, headers=headers, data=img).json()
-        print(result)
-        if result is None:
+        logging.info("grabbing face")
+        image = Image.open(imgPath).convert("RGB")
+        face = faceGrabber.grab_faces(np.array(image))
+        if face is None:
             logging.info(colored("no face, no swipe right. GET OUTTA HERE!", "red"))
+            focus_ffox()
             subprocess.run(["xdotool", "key", "Left"])
             continue
-        logging.info(f'Classification result: {result[0]["label"]}')
-        print(result[0])
+        face = Image.fromarray(face)
+        face.save("face.png")
+        face.show()
+        with open("face.png", "rb") as f:
+            face = f.read()
+        logging.info(f'Classifying image at {imgPath}')
+        result = requests.post(APIURL, headers=headers, data=face).json()
+        if result[0]["label"] == "pos" and result[0]["score"] < 0.9:
+            logging.info(colored(f'Classification result: {result[0]["label"]} with score {result[0]["score"]}', "green"))
+        elif result[0]["label"] == "pos" and result[0]["score"] >= 0.9:
+            logging.info(colored(f'Classification result: {result[0]["label"]} with score {result[0]["score"]}', "blue"))
+        else:
+            logging.info(colored(f'Classification result: {result[0]["label"]} with score {result[0]["score"]}', "red"))
         if result[0]["label"] == "pos" and result[0]["score"] >= 0.9:
             logging.info(colored("pos and confident enough, swiping right", "green"))
             subprocess.run("pw-play", "right.wav")
+            focus_ffox()
             subprocess.run(["xdotool", "key", "Right"])
             newImgPath = os.path.join(posDir, f"{uuid4()}.png")
             while os.path.exists(newImgPath):
                 newImgPath = os.path.join(posDir, f"{uuid4()}.png")
             os.rename(imgPath, newImgPath)
         else:
+            focus_ffox()
             subprocess.run(["xdotool", "key", "Left"])
             logging.info("not pos or pos but not confident enough, swiping left")
             newImgPath = os.path.join(negDir, f"{uuid4()}.png")
